96364165b97f8d7d076961153547e4a186d6a2d8
ggolish <ggolish@sycamores.indstate.edu>
2018-06-02 15:53:00 -0400

    Shortened code.

--- src/morse_translate.c
+++ src/morse_translate.c
@@ -41,138 +41,82 @@ char *morse_translate(const char *plaintext)
         short_gap();
         switch(tolower(token.character)) {
           case 'a':
-            dot();
-            dash();
+            dot(); dash();
             break;
           case 'b':
-            dash();
-            dot();
-            dot();
-            dot();
+            dash(); dot(); dot(); dot();
             break;
           case 'c':
-            dash();
-            dot();
-            dash();
-            dot();
+            dash(); dot(); dash(); dot();
             break;
           case 'd':
-            dash();
-            dot();
-            dot();
+            dash(); dot(); dot();
             break;
           case 'e':
             dot();
             break;
           case 'f':
-            dot();
-            dot();
-            dash();
-            dot();
+            dot(); dot(); dash(); dot();
             break;
           case 'g':
-            dash();
-            dash();
-            dot();
+            dash(); dash(); dot();
             break;
           case 'h':
-            dot();
-            dot();
-            dot();
-            dot();
+            dot(); dot(); dot(); dot();
             break;
           case 'i':
-            dot();
-            dot();
+            dot(); dot();
             break;
           case 'j':
-            dot();
-            dash();
-            dash();
-            dash();
+            dot(); dash(); dash(); dash();
             break;
           case 'k':
-            dash();
-            dot();
-            dash();
+            dash(); dot(); dash();
             break;
           case 'l':
-            dot();
-            dash();
-            dot();
-            dot();
+            dot(); dash(); dot(); dot();
             break;
           case 'm':
-            dash();
-            dash();
+            dash(); dash();
             break;
           case 'n':
-            dash();
-            dot();
+            dash(); dot();
             break;
           case 'o':
-            dash();
-            dash();
-            dash();
+            dash(); dash(); dash();
             break;
           case 'p':
-            dot();
-            dash();
-            dash();
-            dot();
+            dot(); dash(); dash(); dot();
             break;
           case 'q':
-            dash();
-            dash();
-            dot();
-            dash();
+            dash(); dash(); dot(); dash();
             break;
           case 'r':
-            dot();
-            dash();
-            dot();
+            dot(); dash(); dot();
             break;
           case 's':
-            dot();
-            dot();
-            dot();
+            dot(); dot(); dot();
             break;
           case 't':
             dash();
             break;
           case 'u':
-            dot();
-            dot();
-            dash();
+            dot(); dot(); dash();
             break;
           case 'v':
-            dot();
-            dot();
-            dot();
-            dash();
+            dot(); dot(); dot(); dash();
             break;
           case 'w':
-            dot();
-            dash();
-            dash();
+            dot(); dash(); dash();
             break;
           case 'x':
-            dash();
-            dot();
-            dot();
-            dash();
+            dash(); dot(); dot(); dash();
             break;
           case 'y':
-            dash();
-            dot();
-            dash();
-            dash();
+            dash(); dot(); dash(); dash();
             break;
           case 'z':
-            dash();
-            dash();
-            dot();
-            dot();
+            dash(); dash(); dot(); dot();
             break;
           default:
             break;
@@ -185,74 +129,34 @@ char *morse_translate(const char *plaintext)
         short_gap();
         switch(token.character) {
           case '0':
-            dash();
-            dash();
-            dash();
-            dash();
-            dash();
+            dash(); dash(); dash(); dash(); dash();
             break;
           case '1':
-            dot();
-            dash();
-            dash();
-            dash();
-            dash();
+            dot(); dash(); dash(); dash(); dash();
             break;
           case '2':
-            dot();
-            dot();
-            dash();
-            dash();
-            dash();
+            dot(); dot(); dash(); dash(); dash();
             break;
           case '3':
-            dot();
-            dot();
-            dot();
-            dash();
-            dash();
+            dot(); dot(); dot(); dash(); dash();
             break;
           case '4':
-            dot();
-            dot();
-            dot();
-            dot();
-            dash();
+            dot(); dot(); dot(); dot(); dash();
             break;
           case '5':
-            dot();
-            dot();
-            dot();
-            dot();
-            dot();
+            dot(); dot(); dot(); dot(); dot();
             break;
           case '6':
-            dash();
-            dot();
-            dot();
-            dot();
-            dot();
+            dash(); dot(); dot(); dot(); dot();
             break;
           case '7':
-            dash();
-            dash();
-            dot();
-            dot();
-            dot();
+            dash(); dash(); dot(); dot(); dot();
             break;
           case '8':
-            dash();
-            dash();
-            dash();
-            dot();
-            dot();
+            dash(); dash(); dash(); dot(); dot();
             break;
           case '9':
-            dash();
-            dash();
-            dash();
-            dash();
-            dot();
+            dash(); dash(); dash(); dash(); dot();
             break;
           default:
             break;
